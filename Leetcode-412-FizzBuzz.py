676cfe3944a7670fbf76314ec31bde279d619567
kg2407 <iamkg2407@gmail.com>
2021-01-31 15:03:53 +0530

    FizzBuzz-Leetcode-412 solution

---
+++ Leetcode-412-FizzBuzz.py
@@ -0,0 +1,14 @@
+#https://leetcode.com/problems/fizz-buzz/
+class Solution:
+    def fizzBuzz(self, n: int) :
+        ans = []
+        for num in range(1,n+1):
+            if num%15 ==0:
+                ans.append('FizzBuzz')
+            elif num%3 ==0:
+                ans.append('Fizz')
+            elif num%5 == 0:
+                ans.append('Buzz')
+            else:
+                ans.append(str(num))
+        return ans
