f1bb49a8c3066dcd8870030cf3e2f9729acd05e2
kg2407 <iamkg2407@gmail.com>
2021-02-02 10:51:51 +0530

    LC-05-Solution

---
+++ Medium/LC-5.py
@@ -0,0 +1,19 @@
+#https://leetcode.com/problems/longest-palindromic-substring/
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        def helper(l, r):
+            while (l>=0 and r < len(s) and s[l] == s[r]):
+                l -= 1
+                r += 1
+            return s[l+1:r]
+        res = ""
+        for i in range(len(s)):
+            test = helper(i,i)
+            if len(test) > len(res): res = test
+            test = helper(i,i+1)
+            if len(test) > len(res): res =test
+        return res
