24e6b7a94b20f915adbda136dc47f2b62e4f0f94
kg2407 <iamkg2407@gmail.com>
2021-02-06 11:04:40 +0530

    Simplify Path LC - 71 solution

---
+++ Medium/LC-71.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/simplify-path/
+    class Solution:
+    def simplifyPath(self, path: str) -> str:
+        p = path.split("/")
+        p = [a for a in p if a != ""]
+        stack = []
+        for a in p:
+            if a == ".":
+                continue
+            elif a == "..":
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(a)
+        return "/"+"/".join(stack)
